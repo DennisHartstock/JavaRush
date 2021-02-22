51a9fe33c49daae6315bb5e6353118567ed84805
DennsHartstock <dennis.hartstock@web.de>
2021-02-22 07:40:34 +0100

    Когда корабли подлетают к краю экрана, они меняют направление на противоположное и опускаются ниже. Для этого нам нужно реализовать метод move класса EnemyFleet.
    Требования:
    1. Метод move() класса EnemyFleet ничего не делает, если в списке ships нет кораблей.
    2. Если значение поля direction равно Direction.LEFT и результат вызова метода getLeftBorder() отрицательный, полю direction необходимо установить значение Direction.RIGHT.
    3. Если значение поля direction равно Direction.RIGHT и результат вызова метода getRightBorder() больше, чем (SpaceInvadersGame.WIDTH), полю direction необходимо установить значение Direction.LEFT.
    4. В методе move() необходимо вызвать метод getSpeed().
    5. Если значение поля direction было изменено, у каждого объекта из списка ships необходимо вызвать метод move(Direction, double) с параметрами: Direction.DOWN, результат вызова метода getSpeed().
    6. Если значение поля direction не было изменено, у каждого объекта из списка ships необходимо вызвать метод move(Direction, double) с параметрами: direction, результат вызова метода getSpeed().

---
+++ 8.Games/src/com/javarush/games/spaceinvaders/gameobjects/EnemyFleet.java
@@ -0,0 +1,80 @@
+package com.javarush.games.spaceinvaders.gameobjects;
+
+import com.javarush.engine.cell.Game;
+import com.javarush.games.spaceinvaders.Direction;
+import com.javarush.games.spaceinvaders.ShapeMatrix;
+import com.javarush.games.spaceinvaders.SpaceInvadersGame;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EnemyFleet {
+    private static final int ROWS_COUNT = 3;
+    private static final int COLUMNS_COUNT = 10;
+    private static final int STEP = ShapeMatrix.ENEMY.length + 1;
+    private List<EnemyShip> ships;
+    private Direction direction = Direction.RIGHT;
+
+    public EnemyFleet() {
+        createShips();
+    }
+
+    public void draw(Game game) {
+        for (EnemyShip ship : ships) {
+            ship.draw(game);
+        }
+    }
+
+    private void createShips() {
+        ships = new ArrayList<>();
+        for (int x = 0; x < COLUMNS_COUNT; x++) {
+            for (int y = 0; y < ROWS_COUNT; y++) {
+                ships.add(new EnemyShip(x * STEP, y * STEP + 12));
+            }
+        }
+    }
+
+    private double getLeftBorder() {
+        double left = SpaceInvadersGame.WIDTH;
+        for (GameObject ship : ships) {
+            if (ship.x < left) {
+                left = ship.x;
+            }
+        }
+        return left;
+    }
+
+    private double getRightBorder() {
+        double right = 0;
+        for (GameObject ship : ships) {
+            if (ship.x + ship.width > right) {
+                right = ship.x + ship.width;
+            }
+        }
+        return right;
+    }
+
+    private double getSpeed() {
+        return Math.min(2.0, 3.0 / ships.size());
+    }
+
+    public void move() {
+        if (ships.isEmpty()) {
+            return;
+        }
+        
+        Direction currentDirection = direction;
+            if (direction == Direction.LEFT && getLeftBorder() < 0) {
+                direction = Direction.RIGHT;
+                currentDirection = Direction.DOWN;
+            } else if (direction == Direction.RIGHT && getRightBorder() > SpaceInvadersGame.WIDTH) {
+                direction = Direction.LEFT;
+                currentDirection = Direction.DOWN;
+            }
+            
+            double speed = getSpeed();
+            for (EnemyShip ship : ships) {
+                ship.move(currentDirection, speed);
+        }
+    }
+}
---
+++ 8.Games/src/com/javarush/games/spaceinvaders/gameobjects/EnemyShip.java
@@ -0,0 +1,25 @@
+package com.javarush.games.spaceinvaders.gameobjects;
+
+import com.javarush.games.spaceinvaders.Direction;
+import com.javarush.games.spaceinvaders.ShapeMatrix;
+
+public class EnemyShip extends Ship {
+    public EnemyShip(double x, double y) {
+        super(x, y);
+        setStaticView(ShapeMatrix.ENEMY);
+    }
+
+    public void move(Direction direction, double speed) {
+        if (direction == Direction.RIGHT) {
+            x = x + speed;
+        }
+
+        if (direction == Direction.LEFT) {
+            x = x - speed;
+        }
+
+        if (direction == Direction.DOWN) {
+            y += 2;
+        }
+    }
+}
