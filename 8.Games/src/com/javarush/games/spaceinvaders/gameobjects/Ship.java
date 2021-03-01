41688000c71b5dbe5cf6286d58a79aacc6525a8c
DennsHartstock <dennis.hartstock@web.de>
2021-03-01 08:20:05 +0100

    На данном этапе мы подготовим анимацию для крушения кораблей. Для этого нам понадобится поле frames типа List<int[][]>, которое будет хранить список матриц для кадров анимации. Также нам понадобится поле frameIndex, которое хранит индекс текущего кадра анимации. Передавать кадры анимации мы будем в методе setAnimatedView. Он принимает массив кадров(матриц) и передает их списку frames. В завершение, реализуем метод setStaticView, который задает изначальную матрицу для кораблей.
    Требования:
    1. В классе Ship должно существовать приватное поле frames типа List<int[][]>.
    2. В классе Ship должно существовать приватное поле int frameIndex.
    3. В классе Ship должен существовать публичный метод void setAnimatedView(int[][]... viewFrames).
    4. В методе setAnimatedView(int[][]... viewFrames) необходимо вызвать метод setMatrix(int[][]) базового класса. В качестве параметра передай в метод viewFrames[0].
    5. В методе setAnimatedView(int[][]... viewFrames) поле frames должно быть инициализировано значением Arrays.asList(viewFrames).
    6. В методе setAnimatedView(int[][]... viewFrames) полю frameIndex необходимо установить значение 0.
    7. В методе setStaticView(int[][] viewFrame) поле frames должно быть инициализировано новым объектом типа ArrayList<int[][]>.
    8. В методе setStaticView(int[][] viewFrame) в список frames необходимо добавить объект viewFrame.
    9. В методе setStaticView(int[][]) полю frameIndex необходимо установить значение 0.

--- 8.Games/src/com/javarush/games/spaceinvaders/gameobjects/Ship.java
+++ 8.Games/src/com/javarush/games/spaceinvaders/gameobjects/Ship.java
@@ -1,7 +1,13 @@
 package com.javarush.games.spaceinvaders.gameobjects;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Ship extends GameObject {
     public boolean isAlive = true;
+    private List<int[][]> frames;
+    private int frameIndex;
 
     public Ship(double x, double y) {
         super(x, y);
@@ -9,6 +15,9 @@ public class Ship extends GameObject {
 
     public void setStaticView(int[][] viewFrame) {
         setMatrix(viewFrame);
+        frames = new ArrayList<>();
+        frames.add(viewFrame);
+        frameIndex = 0;
     }
 
     public Bullet fire() {
@@ -17,5 +26,12 @@ public class Ship extends GameObject {
 
     public void kill() {
         isAlive = false;
+    }
+
+    public void setAnimatedView(int[][]... viewFrames) {
+        setMatrix(viewFrames[0]);
+
+        frames = Arrays.asList(viewFrames);
+        frameIndex = 0;
     }
 }
