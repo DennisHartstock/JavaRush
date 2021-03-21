b791d46fd03ddd2164b17f02813978eaa4e294b7
DennsHartstock <dennis.hartstock@web.de>
2021-03-21 09:56:20 +0100

    Игра змейка (9/20) Движение змейки очень простое: в соседней с головой ячейке создается новая голова, а последний элемент (хвост) змейки удаляется.
    В классе Snake реализуй метод createNewHead(), который создает новый элемент GameObject и возвращает его. Новый элемент GameObject должен создаваться рядом с элементом, в котором на данный момент находится голова змейки (элемент с индексом 0 из snakeParts).
    
    Новый элемент GameObject создается с той стороны, куда движется змейка (поле direction).
    
    Пример:
    
    - если змейка движется влево, new GameObject(headX-1, headY);
    - если змейка движется вниз, new GameObject(headX, headY + 1)
    
    Здесь headX — координата головы змеи x, headY — координата головы змеи y.
    
    Примечание: координаты могут быть отрицательные и/или быть за границами игрового поля. Пока что это нормально, в дальнейших частях задачи исправим.
    
    В классе Snake создай метод removeTail() и реализуй его.
    
    Требования:
    1. В классе Snake должен существовать публичный метод GameObject createNewHead().
    2. Метод createNewHead() должен быть реализован согласно условию.
    3. В классе Snake должен существовать публичный метод void removeTail().
    4. Метод removeTail() должен удалять последний элемент из списка snakeParts.

--- 8.Games/src/com/javarush/games/snake/Snake.java
+++ 8.Games/src/com/javarush/games/snake/Snake.java
@@ -6,10 +6,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Snake {
-    
+
     private final static String HEAD_SIGN = "\uD83D\uDC7E";
     private final static String BODY_SIGN = "\u26AB";
-    
+
     private List<GameObject> snakeParts = new ArrayList<>();
     public boolean isAlive = true;
     private Direction direction = Direction.LEFT;
@@ -22,7 +22,7 @@ public class Snake {
 
     public void draw(Game game) {
         Color color = isAlive ? Color.BLACK : Color.RED;
-        
+
         for (int i = 0; i < snakeParts.size(); i++) {
             GameObject part = snakeParts.get(i);
             String sign = (i != 0) ? BODY_SIGN : HEAD_SIGN;
@@ -35,5 +35,22 @@ public class Snake {
 
     public void setDirection(Direction direction) {
         this.direction = direction;
+    }
+
+    public GameObject createNewHead() {
+        GameObject oldHead = snakeParts.get(0);
+        if (direction == Direction.LEFT) {
+            return new GameObject(oldHead.x - 1, oldHead.y);
+        } else if (direction == Direction.RIGHT) {
+            return new GameObject(oldHead.x + 1, oldHead.y);
+        } else if (direction == Direction.UP) {
+            return new GameObject(oldHead.x, oldHead.y - 1);
+        } else {
+            return new GameObject(oldHead.x, oldHead.y + 1);
+        }
+    }
+
+    public void removeTail() {
+        snakeParts.remove(snakeParts.size() - 1);
     }
 }
