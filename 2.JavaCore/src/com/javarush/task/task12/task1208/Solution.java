cc74be2ee3cb2f3422d1a7c8c2f5ff5694c4cedf
DennsHartstock <dennis.hartstock@web.de>
2021-09-08 08:00:51 +0200

    Свобода печати Напиши пять методов print() с разными параметрами.
    Requirements:
    1. Программа не должна выводить текст на экран.
    2. Класс Solution должен содержать шесть методов.
    3. Класс Solution должен содержать статический метод main().
    4. Класс Solution должен содержать пять статических методов print() с разными параметрами.

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print(int a) {
+
+    }
+
+    public static void print(String string) {
+
+    }
+
+    public static void print(boolean b) {
+
+    }
+
+    public static void print(char c) {
+        
+    }
+    
+    public static void print(long v) {
+        
+    }
+}
