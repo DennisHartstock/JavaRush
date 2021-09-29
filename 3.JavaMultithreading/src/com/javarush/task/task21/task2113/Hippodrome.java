7b58bf013ee6ee54e75b528950d3b0ffd7da6f95
DennsHartstock <dennis.hartstock@web.de>
2021-09-29 15:59:03 +0200

    Ипподром(7) Но и это еще не все - надо чтобы лошади бежали. Добавь в класс Hippodrome void методы run, move и print. Без параметров. Метод move будет управлять движением всех лошадей. Метод print отрисовывать их на экран. А метод run - управлять всем этим.
    Requirements:
    1. В классе Hippodrome должен быть создан метод void run() без параметров.
    2. В классе Hippodrome должен быть создан метод void move() без параметров.
    3. В классе Hippodrome должен быть создан метод void print() без параметров.

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -1,9 +1,23 @@
 package com.javarush.task.task21.task2113;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Hippodrome {
+    static Hippodrome game;
     private List<Horse> horses;
+
+    void run() {
+
+    }
+
+    void move() {
+
+    }
+
+    void print() {
+
+    }
 
     public Hippodrome(List<Horse> horses) {
         this.horses = horses;
@@ -14,6 +28,9 @@ public class Hippodrome {
     }
 
     public static void main(String[] args) {
-
+        game = new Hippodrome(new ArrayList<>());
+        game.getHorses().add(new Horse("One", 3, 0));
+        game.getHorses().add(new Horse("Two", 3, 0));
+        game.getHorses().add(new Horse("Three", 3, 0));
     }
 }
