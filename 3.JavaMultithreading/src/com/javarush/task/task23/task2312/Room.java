66516c155822912db7c78e71081e52899790d7b6
DennsHartstock <dennis.hartstock@web.de>
2021-10-04 07:43:26 +0200

    Змейка(14) Теперь поработаем над методом print().
    Надо:
    а) вывести на экран прямоугольник из точек размером width x height.
    б) тело змеи отметить символом "x"-английское
    в) голову змеи нарисовать символом "X"-английское.
    
    Подсказка:
    а) удобно сначала создать матрицу типа int[][] с размером (height x width)
    б) затем пройтись по всем объектам и отрисовать их в матрицу.
    Например, тело змеи - 1, голова змеи - 2, мышь - 3.
    
    Requirements:
    1. Метод print должен выводить данные на экран.
    2. Метод print должен выводить на экран количество строк равное height.
    3. Количество символов в каждой строке выведенной на экран должно быть равно width.
    4. Голова змеи должна быть отмечена символом X.
    5. Тело змеи должно быть отмечено символами x.
    6. Мышь должна быть отмечена символом ^.

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Room.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Room.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task23.task2312;
 
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
 
 public class Room {
     private int width;
@@ -91,9 +92,30 @@ public class Room {
      */
     public void print() {
         //Создаем массив, куда будем "рисовать" текущее состояние игры
+        int[][] matrix = new int[height][width];
+
         //Рисуем все кусочки змеи
+        ArrayList<SnakeSection> sections = new ArrayList<>(snake.getSections());
+        for (SnakeSection snakeSection : sections) {
+            matrix[snakeSection.getY()][snakeSection.getX()] = 1;
+        }
+
+        matrix[snake.getY()][snake.getX()] = snake.isAlive() ? 2 : 4;
+
         //Рисуем мышь
+        matrix[mouse.getY()][mouse.getX()] = 3;
+
         //Выводим все это на экран
+        String[] symbols = {".", "x", "X", "^", "*"};
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                System.out.print(symbols[matrix[y][x]]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println();
+        System.out.println();
     }
 
     /**
@@ -125,14 +147,14 @@ public class Room {
 
     private int initialDelay = 520;
     private int delayStep = 20;
-    
+
     public void sleep() {
         try {
             int level = snake.getSections().size();
             int delay = level < 15 ? (initialDelay - delayStep * level) : 200;
             Thread.sleep(delay);
-        } catch (InterruptedException e0) {
-            
+        } catch (InterruptedException e) {
+
         }
     }
 }
