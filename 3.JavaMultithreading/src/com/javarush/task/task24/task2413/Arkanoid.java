443f2f5a866b2e4ec1cd66969fa8a9d0425a3379
DennsHartstock <dennis.hartstock@web.de>
2021-10-08 07:29:25 +0200

    Арканоид(4) Также классу Arkanoid нужно будет хранить ссылку на шарик (Ball), "подставку" (Stand) и список "кирпичей" (List<Brick>).
    Нужно в классе Arkanoid:
    а) создать приватное поле ball типа Ball
    б) создать приватное поле stand типа Stand
    в) создать приватное поле bricks типа List<Brick>
    г) добавить для них публичные геттеры и сеттеры
    
    Requirements:
    1. В классе Arkanoid должно быть создано приватное поле ball типа Ball.
    2. В классе Arkanoid должно быть создано приватное поле stand типа Stand.
    3. В классе Arkanoid должно быть создано приватное поле bricks типа List<Brick>.
    4. В классе Arkanoid должен быть создан корректный геттер для поля ball.
    5. В классе Arkanoid должен быть создан корректный геттер для поля stand.
    6. В классе Arkanoid должен быть создан корректный геттер для поля bricks.
    7. В классе Arkanoid должен быть создан корректный сеттер для поля ball.
    8. В классе Arkanoid должен быть создан корректный сеттер для поля stand.
    9. В классе Arkanoid должен быть создан корректный сеттер для поля bricks.

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
@@ -1,6 +1,10 @@
 package com.javarush.task.task24.task2413;
 
+import java.util.List;
+
 public class Arkanoid {
+    private int width;
+    private int height;
 
     public int getWidth() {
         return width;
@@ -18,13 +22,39 @@ public class Arkanoid {
         this.height = height;
     }
 
-    private int width;
-    private int height;
-
     public Arkanoid(int width, int height) {
         this.width = width;
         this.height = height;
     }
+
+    private Ball ball;
+    private Stand stand;
+    private List<Brick> bricks;
+
+    public Ball getBall() {
+        return ball;
+    }
+
+    public void setBall(Ball ball) {
+        this.ball = ball;
+    }
+
+    public Stand getStand() {
+        return stand;
+    }
+
+    public void setStand(Stand stand) {
+        this.stand = stand;
+    }
+
+    public List<Brick> getBricks() {
+        return bricks;
+    }
+
+    public void setBricks(List<Brick> bricks) {
+        this.bricks = bricks;
+    }
+
 
     public static void main(String[] args) {
 
