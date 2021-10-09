77da626a37b9085619edc77daa3ca468638136d3
DennsHartstock <dennis.hartstock@web.de>
2021-10-09 08:57:54 +0200

    Арканоид(13) Класс Ball уже посложнее - шарик же двигается.
    Нам понадобятся приватные поля:
    а) speed (скорость шарика) типа double;
    б) direction (направление движения в градусах: от 0 до 360) типа double;
    в) dx (расстояние по x, которое проходит шарик за один шаг. вычисляется на основе speed и direction) типа double;
    г) dy (расстояние по y, которое проходит шарик за один шаг. вычисляется на основе speed и direction) типа double;
    д) isFrozen ("истина" если шарик "заморожен" - не двигается) типа boolean.
    
    Не забудь добавить геттеры для всех полей этого класса, кроме isFrozen (его мы будем использовать только внутри класса Ball).
    
    А еще сделай-ка конструктор:
    а) параметры - x, y, speed, direction;
    б) радиус (для вызова родительского класса) всегда равен 1;
    в) не забудь установить isFrozen в true - в начале игры шарик никуда не летит.
    
    Requirements:
    1. Приватное поле speed типа double должно быть создано в классе Ball.
    2. Приватное поле direction типа double должно быть создано в классе Ball.
    3. Приватное поле dx типа double должно быть создано в классе Ball.
    4. Приватное поле dy типа double должно быть создано в классе Ball.
    5. Приватное поле isFrozen типа boolean должно быть создано в классе Ball.
    6. В классе Ball должен быть создан публичный конструктор (поведение и параметры описаны в условии).
    7. В классе Ball должен существовать корректный публичный геттер для поля speed.
    8. В классе Ball должен существовать корректный публичный геттер для поля direction.
    9. В классе Ball должен существовать корректный публичный геттер для поля dx.
    10. В классе Ball должен существовать корректный публичный геттер для поля dy.

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
@@ -1,11 +1,35 @@
 package com.javarush.task.task24.task2413;
 
 public class Ball extends BaseObject {
+    private double speed;
+    private double direction;
+    private double dx;
+    private double dy;
+    private boolean isFrozen;
 
-    public Ball(double x, double y, double radius) {
-        super(x, y, radius);
+    public double getSpeed() {
+        return speed;
     }
 
+    public double getDirection() {
+        return direction;
+    }
+
+    public double getDx() {
+        return dx;
+    }
+
+    public double getDy() {
+        return dy;
+    }
+
+    public Ball(double x, double y, double speed, double direction) {
+        super(x, y, 1);
+        this.speed = speed;
+        this.direction = direction;
+        this.isFrozen = true;
+    }
+   
     @Override
     void draw(Canvas canvas) {
 
