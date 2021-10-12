5a7a0373874c5e4ff4e219501c00913c6553e6d2
DennsHartstock <dennis.hartstock@web.de>
2021-10-12 08:09:51 +0200

    MVC (8) 1. Следуя принципу MVC аналогично реализации предыдущих методов сделай следующее: напиши логику удаления пользователя. После удаления должен отображаться список пользователей.
    Распредели следующие методы по классам MVC:
    public void fireEventUserDeleted(long id)
    public void onUserDelete(long id)
    public void deleteUserById(long id)
    
    Примечание: метод, который ты собираешься добавить в Вью нужно добавить в EditUserView.
    
    2. Добавь в main вызов fireEventUserDeleted(124L) перед вызовом метода fireEventShowDeletedUsers().
    
    3. Выполни рефакторинг класса MainModel. Теперь, когда есть удаленные пользователи, часть методов стала работать неправильно.
    Почти во всех методах, где требуется список пользователей, нужно работать только с активными(живыми) пользователями.
    Вынеси в отдельный приватный метод List<User> getAllUsers() получение списка всех активных пользователей.
    Фильтрация активных пользователей у тебя уже есть - метод List<User> filterOnlyActiveUsers(List<User> allUsers).
    Отрефактори все методы, которые используют список пользователей. Они должны использовать список живых пользователей.
    
    4. Добавь в интерфейс Model метод, который ты поместил в Модель, реализуй его в FakeModel: выброси UnsupportedOperationException.
    
    Requirements:
    1. Необходимо определить правильное расположение метода fireEventUserDeleted(long) и реализовать этот метод.
    2. Необходимо определить правильное расположение метода onUserDelete(long) и реализовать этот метод.
    3. Необходимо определить правильное расположение метода deleteUserById(long) и реализовать этот метод.
    4. В методе main класса Solution необходимо вызвать метод fireEventUserDeleted(124L) у объекта класса EditUserView перед вызовом метода fireEventShowDeletedUsers().
    5. В методе main вызов методов должен происходить в такой последовательности: fireEventShowAllUsers(), fireEventOpenUserEditForm(126L), fireEventUserDeleted(124L), fireEventShowDeletedUsers().
    6. Необходимо реализовать приватный метод List<User> getAllUsers() в классе MainModel.
    7. Необходимо выполнить рефакторинг: все методы класса MainModel, которые используют список пользователей, должны использовать список живых пользователей.
    8. Интерфейс Model должен содержать объявление метода, который ты ранее реализовал в классе MainModel.
    9. В классе FakeModel в теле метода, помещенного в интерфейс Model, необходимо бросить UnsupportedOperationException.

--- 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/MainModel.java
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/MainModel.java
@@ -19,7 +19,7 @@ public class MainModel implements Model {
 
     @Override
     public void loadUsers() {
-        List<User> users = userService.getUsersBetweenLevels(1, 100);
+        List<User> users = getAllUsers();
         modelData.setUsers(users);
         modelData.setDisplayDeletedUserList(false);
     }
@@ -35,5 +35,18 @@ public class MainModel implements Model {
     public void loadUserById(long userId) {
         User user = userService.getUsersById(userId);
         modelData.setActiveUser(user);
+    }
+
+    @Override
+    public void deleteUserById(long id) {
+        userService.deleteUser(id);
+        List<User> users = getAllUsers();
+        modelData.setUsers(users);
+    }
+
+    private List<User> getAllUsers() {
+        List<User> allUsers = userService.getUsersBetweenLevels(1, 100);
+        allUsers = userService.filterOnlyActiveUsers(allUsers);
+        return allUsers;
     }
 }
