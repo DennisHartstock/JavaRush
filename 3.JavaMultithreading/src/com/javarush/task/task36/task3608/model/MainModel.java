04b3fe470b74bc6b37dbf291ff38f92b8a700238
DennsHartstock <dennis.hartstock@web.de>
2021-10-12 07:23:25 +0200

    MVC (5) В сервисе есть метод, который возвращает всех удаленных пользователей. Давай их отобразим.
    1. Распредели методы по классам MVC:
    
    public void fireEventShowDeletedUsers() {
    ...onShowAllDeletedUsers();
    }
    
    public void onShowAllDeletedUsers() {
    ...loadDeletedUsers();
    }
    
    public void loadDeletedUsers() {
    List<User> users = userService.getAllDeletedUsers();
    }
    
    Не забудь, что данные, полученные с сервера, необходимо положить в ModelData. А потом обновить view.
    Добавь это самостоятельно в нужные методы.
    
    2. Добавь в Solution.main вызов нового метода, который ты поместил в view.
    
    3. Добавь в интерфейс Model метод, который ты поместил в Модель, реализуй его в FakeModel: выброси UnsupportedOperationException.
    
    Requirements:
    1. Необходимо определить правильное расположение метода fireEventShowDeletedUsers() и реализовать этот метод.
    2. Необходимо определить правильное расположение метода onShowAllDeletedUsers() и реализовать этот метод.
    3. Необходимо определить правильное расположение метода loadDeletedUsers() и реализовать этот метод.
    4. В методе main класса Solution необходимо вызвать метод, который ты ранее реализовал в классе UsersView.
    5. Интерфейс Model должен содержать объявление метода, который ты ранее реализовал в классе MainModel.
    6. В классе FakeModel в теле метода, помещенного в интерфейс Model, необходимо бросить UnsupportedOperationException.

--- 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/MainModel.java
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/MainModel.java
@@ -7,17 +7,25 @@ import com.javarush.task.task36.task3608.model.service.UserServiceImpl;
 import java.util.List;
 
 public class MainModel implements Model {
-    private ModelData modelData = new ModelData();
+
     private UserService userService = new UserServiceImpl();
+
+    private ModelData modelData = new ModelData();
 
     @Override
     public ModelData getModelData() {
         return modelData;
     }
-    
+
     @Override
     public void loadUsers() {
         List<User> users = userService.getUsersBetweenLevels(1, 100);
+        modelData.setUsers(users);
+    }
+
+    @Override
+    public void loadDeletedUsers() {
+        List<User> users = userService.getAllDeletedUsers();
         modelData.setUsers(users);
     }
 }
