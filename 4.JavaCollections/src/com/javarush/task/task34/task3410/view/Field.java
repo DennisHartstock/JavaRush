4b435cf9cb07c92345d40dfe046620a89981e88a
DennsHartstock <dennis.hartstock@web.de>
2021-11-26 10:01:23 +0100

    Sokoban (12) Попробуем организовать взаимодействие представления и модели. 12.1. Добавь в класс View метод update(), он должен вызывать у игрового поля field метод repaint(). Другими словами, метод update() будет обновлять представление (перерисовывать поле). 12.2. Добавь в класс контроллера метод GameObjects getGameObjects(), он должен запросить игровые объекты у модели и вернуть их. Добавь такой же метод и в класс представления, он должен получать объекты у контроллера. 12.3. Правильно реализуй в классе Field метод paint(Graphics g). Он должен: 12.3.1. Залить все поле каким-то цветом, например, черным (вызови методы setColor и fillRect). 12.3.2. Получить у представления все игровые объекты. 12.3.3. Отрисовать все игровые объекты. 12.4. Контроллер в своем конструкторе должен перезапускать модель.
    Запусти программу и проверь, что все игровые объекты рисуются правильно.
    
    Requirements:
    1. Реализуй в классе View метод update().
    2. Реализуй в классе контроллера метод GameObjects getGameObjects().
    3. Реализуй в классе Field метод paint(Graphics g).
    4. Контроллер в своем конструкторе должен перезапускать модель.

--- 4.JavaCollections/src/com/javarush/task/task34/task3410/view/Field.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/view/Field.java
@@ -1,9 +1,11 @@
 package com.javarush.task.task34.task3410.view;
 
 import com.javarush.task.task34.task3410.controller.EventListener;
+import com.javarush.task.task34.task3410.model.GameObject;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Set;
 
 public class Field extends JPanel {
     View view;
@@ -20,6 +22,13 @@ public class Field extends JPanel {
 
     @Override
     public void paint(Graphics g) {
+        g.setColor(Color.black);
+        g.fillRect(0, 0, getWidth(), getHeight());
 
+        Set<GameObject> gameObjects = view.getGameObjects().getAll();
+
+        for (GameObject gameObject : gameObjects) {
+            gameObject.draw(g);
+        }
     }
 }
